6f47fdc2fe21fa519823477dad8e1f6c239635e8
MSI\9blue <9.bluelake@gmail.com>
2023-07-19 19:01:38 +0630

    product.php create

--- Customer/View/index.php
+++ Customer/View/index.php
@@ -12,7 +12,6 @@
 
     <!-- tailwind  link -->
     <link href="./assets/tailwind/output.css" rel="stylesheet" />
-    <script src="https://cdn.tailwindcss.com"></script>
  
     <!-- flow bite link -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.7.0/flowbite.min.js"></script>
