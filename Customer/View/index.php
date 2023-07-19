af9161a8cca5334484e575d0659092c0fdcb2b1a
MSI\9blue <9.bluelake@gmail.com>
2023-07-19 18:38:40 +0630

    Update text

--- Customer/View/index.php
+++ Customer/View/index.php
@@ -5,12 +5,12 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Hello</title>
-
     <link rel="stylesheet" href="./assets/tailwind/output.css" />
 </head>
 
 <body>
     <h1 class="text-red-400">Hello World</h1>
+    <h1 class="text-blue-500">Customer Test</h1>
 </body>
 
 </html>
