dabcb30801d707f4d4c6e8cfebb10b6f2efe4bcb
MSI\9blue <9.bluelake@gmail.com>
2023-07-20 12:32:33 +0630

    Folder Structure Update and Tailwind Config Custom Update

--- Customer/View/tailwind.config.js
+++ Customer/View/tailwind.config.js
@@ -11,7 +11,9 @@ module.exports = {
         'textWhite': "#FFFFFF",
         'textBlack': "#000000",
         'textOrange': "#F36823",
-        'textGray': "#A2A2A2"
+        'textGray': "#A2A2A2",
+        'borderOrange': '#F36823',
+        'productCardBgColor': '#F7F7F7'
       },
       fontFamily: {
         'roboto': ['Roboto', 'sans-serif'],
