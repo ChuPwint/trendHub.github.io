63548bcd07af2e8b24728becdf31d6dd2581ffdb
DESKTOP-UK4Q25M\Ma Thazin Aung <taung2459@gmail.com>
2023-07-20 12:38:03 +0630

    no message

--- Customer/View/tailwind.config.js
+++ Customer/View/tailwind.config.js
@@ -12,7 +12,15 @@ colors: {
       'textBlack' : "#000000",
       'textOrange': "#F36823",
       'textGray' : "#A2A2A2",
+<<<<<<< HEAD
 },
+=======
+      'orangeHover' : "",
+},
+fontFamily: {
+  'roboto' : ['Roboto', 'sans-serif'],
+ },
+>>>>>>> 2628820ecd31000b1ed2326cf5ab00c77a08d772
     },
   },
   plugins: [],
