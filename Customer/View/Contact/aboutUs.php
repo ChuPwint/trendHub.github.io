ce35c8147d69589109655fd445812e0350371275
MSI\9blue <9.bluelake@gmail.com>
2023-07-21 22:47:22 +0630

    update aboutUs.php

--- Customer/View/Contact/aboutUs.php
+++ Customer/View/Contact/aboutUs.php
@@ -9,6 +9,7 @@
     <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
     <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
     <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
+    <!-- <script src="https://cdn.tailwindcss.com"></script> -->
 
 </head>
 
