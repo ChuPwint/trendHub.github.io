b10f1a6cb67202f545fa2853934279a2c0eff7c5
MSI\9blue <9.bluelake@gmail.com>
2023-07-23 22:18:27 +0630

    Admin's dashboard create

---
+++ Admin/View/dashboard.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dashboard</title>
+    <link rel="stylesheet" href="./resources/lib/tailwind/output.css?id=<?= time() ?>">
+</head>
+<body>
+    <section class="container w-full mx-auto">
+        <!-- Left-side Categories -->
+        <div class="bg-[#191C25] text-white">
+            <h1 class="text-[32px] font-medium">Admin</h1>
+        </div>
+
+    </section>
+</body>
+</html>
