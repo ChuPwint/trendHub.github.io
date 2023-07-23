9bcf65aa99a3955a5ea6f160c1aa4d0e2ca0cc02
MSI\9blue <9.bluelake@gmail.com>
2023-07-23 14:58:11 +0630

    contact.php Update

--- Customer/View/Contact/contact.php
+++ Customer/View/Contact/contact.php
@@ -7,7 +7,7 @@
     <title>Contact</title>
     <link rel="stylesheet" href="../resources/lib/tailwind/output.css?id=<?= time() ?>">
     <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
-    <!-- <script src="https://cdn.tailwindcss.com"></script> -->
+    <script src="https://cdn.tailwindcss.com"></script>
 </head>
 
 <body class="bg-secondary md:bg-primary font-roboto">
