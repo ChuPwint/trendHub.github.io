2aa7d99a548f5c8f877990eb46d9b9959bbcdf33
Chu <chupwintchal99@gmail.com>
2023-07-24 23:14:26 +0630

    js update

--- Customer/View/resources/js/product/itemDetail.js
+++ Customer/View/resources/js/product/itemDetail.js
@@ -1,8 +1,8 @@
 var minusBtn = document.getElementsByClassName("minusBtn");
 var plusBtn = document.getElementsByClassName("plusBtn");
 var quantity = document.getElementsByClassName("quantityInput");
-var wishListAdd = document.getElementsByClassName("wishListAdd");
-var heartIcon = document.getElementById("heartIcon");
+// var wishListAdd = document.getElementsByClassName("wishListAdd");
+// var heartIcon = document.getElementById("heartIcon");
 
 var stock = 10;
 
@@ -28,12 +28,14 @@ for (let index = 0; index < minusBtn.length; index++) {
     });
 }
 
-for (let index = 0; index < wishListAdd.length; index++) {
-    wishListAdd[index].addEventListener("click", () => {
-        console.log("inside index " + index);
-        wishListTextAndIconToggle();
-    });
-}
+// for (let index = 0; index < wishListAdd.length; index++) {
+    
+//     wishListAdd[index].addEventListener("click", () => {
+//         console.log(wishListAdd.length);
+//         console.log("inside index " + index);
+//         wishListTextAndIconToggle();
+//     });
+// }
 
 /*
 *Create:Choo Pwint Chal(2023/07/23)
@@ -70,14 +72,14 @@ function enableQtyBtn(enabledBtn) {
 *Parameter: no parameter
 *No Return
 */
-function wishListTextAndIconToggle() {
-    console.log("inside");
-    (wishListAdd[0].classList.contains("text-textOrange")) ? "" : wishListAdd[0].classList.add("text-textOrange");
-    if (wishListAdd[1].innerText == "Add to Wishlist") {
-        wishListAdd[1].innerText = "Added to Wishlist";
-        wishListAdd[1].classList.add("text-textOrange");
-    } else {
-        wishListAdd[1].innerText = "Add to Wishlist";
-        wishListAdd[1].classList.remove("text-textOrange");
-    }
-}
+// function wishListTextAndIconToggle() {
+//     console.log("inside");
+//     (wishListAdd[0].classList.contains("text-textOrange")) ? "" : wishListAdd[0].classList.add("text-textOrange");
+//     if (wishListAdd[1].innerText == "Add to Wishlist") {
+//         wishListAdd[1].innerText = "Added to Wishlist";
+//         wishListAdd[1].classList.add("text-textOrange");
+//     } else {
+//         wishListAdd[1].innerText = "Add to Wishlist";
+//         wishListAdd[1].classList.remove("text-textOrange");
+//     }
+// }
---
+++ Merchant/View/Login/login.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Merchant Login</title>
+    <link rel="stylesheet" href="../resources/lib/tailwind/output.css?id=<?= time() ?>">
+    <link rel="stylesheet" href="../resources/css/background/background.css">
+    <script src="../resources/lib/jquery3.6.0.js"></script>
+</head>
+<body class="merchantBg w-screen h-screen">
+    
+</body>
+</html>
