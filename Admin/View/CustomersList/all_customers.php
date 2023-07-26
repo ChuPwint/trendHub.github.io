7746b9d251a6bcd25601086e208ae7969c921865
MSI\9blue <9.bluelake@gmail.com>
2023-07-26 17:28:46 +0630

    update

--- Admin/View/CustomersList/all_customers.php
+++ Admin/View/CustomersList/all_customers.php
@@ -28,11 +28,110 @@
             <!-- Search End-->
 
 
-            <!-- Analytics Data Start -->
-            <div class="px-20 py-16 data-output">
-                
+            <!-- All Customers Data Start -->
+            <div class="px-10 py-16 data-output">
+                <!-- Sort Start  -->
+                <div class="flex items-center justify-end space-x-2">
+                    <span class="text-white">Sort by:</span>
+                    <select name="" id="" class="outline-none rounded py-1 px-3">
+                        <option value="">Name</option>
+                        <option value="">Address</option>
+                        <option value="">Date</option>
+                    </select>
+                </div>
+                <!-- Sort END  -->
+
+                <!-- Table Start  -->
+                <div class="">
+
+                    <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
+                        <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400 border-separate border-spacing-y-1.5">
+                            <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                                <tr>
+                                    <th scope="col" class="px-6 py-4 p-4 rounded-tl rounded-tr">
+                                        No
+                                    </th>
+                                    <th scope="col" class="px-6 py-4">
+                                        Name
+                                    </th>
+                                    <th scope="col" class="px-6 py-4">
+                                        Email
+                                    </th>
+                                    <th scope="col" class="px-6 py-4">
+                                        Phone No
+                                    </th>
+                                    <th scope="col" class="px-6 py-4">
+                                        Address
+                                    </th>
+                                    <th scope="col" class="px-6 py-4">
+                                        Date
+                                    </th>
+                                    <th scope="col" class="px-6 py-4 ">
+                                        Action
+                                    </th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                                    <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                        Apple MacBook Pro 17"
+                                    </th>
+                                    <td class="px-6 py-4">
+                                        Silver
+                                    </td>
+                                    <td class="px-6 py-4">
+                                        Laptop
+                                    </td>
+                                    <td class="px-6 py-4">
+                                        $2999
+                                    </td>
+                                    <td class="px-6 py-4">
+                                        <a href="#" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a>
+                                    </td>
+                                </tr>
+                                <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                                    <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                        Microsoft Surface Pro
+                                    </th>
+                                    <td class="px-6 py-4">
+                                        White
+                                    </td>
+                                    <td class="px-6 py-4">
+                                        Laptop PC
+                                    </td>
+                                    <td class="px-6 py-4">
+                                        $1999
+                                    </td>
+                                    <td class="px-6 py-4">
+                                        <a href="#" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a>
+                                    </td>
+                                </tr>
+                                <tr class="bg-white dark:bg-gray-800">
+                                    <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                        Magic Mouse 2
+                                    </th>
+                                    <td class="px-6 py-4">
+                                        Black
+                                    </td>
+                                    <td class="px-6 py-4">
+                                        Accessories
+                                    </td>
+                                    <td class="px-6 py-4">
+                                        $99
+                                    </td>
+                                    <td class="px-6 py-4">
+                                        <a href="#" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a>
+                                    </td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+
+                </div>
+                <!-- Table End  -->
+
             </div>
-            <!-- Analytics Data End  -->
+            <!-- All Customers Data End  -->
 
         </div>
         <!-- Right-side End -->
