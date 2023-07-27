e850ce99b7d366ea23992e22945bee6fe26e62ac
DESKTOP-UK4Q25M\Ma Thazin Aung <taung2459@gmail.com>
2023-07-28 01:24:49 +0630

    merchant review update and user profile correction

--- Merchant/View/review/reivew.php
+++ Merchant/View/review/reivew.php
@@ -12,132 +12,206 @@
 </head>
 
 <body class="merchantBg w-screen h-screen">
-    <!--Start of detail card-->
 
-    <div class="bg-white flex flex-col justify-center rounded-md items-center shadow-md w-3/4 ">
 
-        <div class="bg-tertiary w-full p-8 m-4 rounded-md flex flex-row justify-between">
 
-            <!-- Product Details -->
-            <div class=" flex flex-row">
-                <div id="productDetail" class="flex-shrink-0">
-                    <img class="w-24 mt-8" src="../resources/img/reviewProduct/download 2.png" alt="T-shirt">
-                </div>
-                <div class="flex flex-col justify-center ml-4">
-                    <div class="font-bold text-xl mb-2">T-shirt</div>
-                    <div class="text-gray-700">
-                        <p>
-                            <span class="font-semibold">Stock:</span> 20
-                        </p>
-                        <p>
-                            <span class="font-semibold">Price:</span> $750.00
-                        </p>
+
+    <!-- start review -->
+    <div id="modalReview" class="modal  fixed inset-0 bg-gray-900 bg-opacity-50 flex justify-center items-center">
+        <div class="modal-content bg-[#FEFEFE] w-[1000px] h-[650px] rounded shadow-md relative">
+            <span class=" font-semibold text-lg px-5 block mt-3 ">Item's Reviews</span>
+            <button onclick="hideReview(),hideDetail()" class="absolute top-4 right-4 text-gray-700 hover:text-gray-900">
+                <svg class="h-6 w-6 text-[#F36823] " fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12"></path>
+                </svg>
+            </button>
+
+            <div class="w-[950px] h-[180px] mt-[25px] mx-auto flex justify-between">
+                <div class="flex">
+                    <div class="w-32">
+                        <img src="../resources/img/reviewProduct/download 2.png" alt="">
+                    </div>
+                    <div class="text-md font-semibold ml-2 ">
+                        <span>Name : T-shirt</span><br>
+                        <span>Stock : 20</span><br>
+                        <span>Price : $750.00</span>
                     </div>
                 </div>
-            </div>
 
-            <!-- Review Summary -->
-            <div class=" ml-4">
-                <p class="text-xl font-bold underline">Rating & Reviews</p>
-                <div class="mt-3 flex justify-between items-center">
-                    <!-- Summary of review of the item -->
-                    <div class="md:flex-none md:mr-5">
-                        <p class="font-bold text-lg"><span class="rating text-4xl">4.0</span> / 5.0</p>
-                        <p><span class="total_rating">20</span> ratings</p>
-                    </div>
-
-                    <!-- Stars and how many stars given -->
-                    <div class="md:flex-grow md:border-r md:border-gray-500">
-                        <!-- ... Review stars HTML here ... -->
-                        <div class="flex justify-start items-center mb-3">
-                            <img class="w-24" src="../resources/img/reviewProduct/5stars.svg" alt="">
-                            <div class="rating_five text-lg mx-3"></div>
-                        </div>
-                        <div class="flex justify-start items-center mb-3">
-                            <img class="w-24" src="../resources/img/reviewProduct/4stars.svg" alt="">
-                            <div class="rating_four text-lg mx-3">15</div>
-                        </div>
-                        <div class="flex justify-start items-center mb-3">
-                            <img class="w-24" src="../resources/img/reviewProduct/3stars.svg" alt="">
-                            <div class="rating_three text-lg mx-3">5</div>
-                        </div>
-                        <div class="flex justify-start items-center mb-3">
-                            <img class="w-24" src="../resources/img/reviewProduct/2stars.svg" alt="">
-                            <div class="rating_two text-lg mx-3"></div>
-                        </div>
-                        <div class="flex justify-start items-center">
-                            <img class="w-24" src="../resources/img/reviewProduct/1star.svg" alt="">
-                            <div class="rating_one text-lg mx-3"></div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-        </div>
-
-
-        <div>
-            <!-- Start of user reviews -->
-            <div class="mt-5">
-                <div class="bg-productCardBgColor py-5 px-5 shadow-md">
-                    <div class="flex justify-between items-center">
-                        <!-- user img and rating star img -->
-                        <div class="flex justify-start items-center">
-                            <img class="w-10" src="../resources/img/reviewProduct/c2 3.png" alt="">
-                            <img class="ml-2 w-24 review_userRatingImg" src="../resources/img/reviewProduct/4stars.svg" alt="">
-                        </div>
-                        <!-- Date review written -->
+                <div class="rating pr-10 w-[230px]">
+                    <h1 class="text-xl font-semibold underline tracking-wider ">Rating & Reviews</h1>
+                    <div class="flex">
                         <div>
-                            <p class="review_createDate text-lg">2023/07/20</p>
+                            <span class="text-2xl font-semibold">4.0/</span>
+                            <span>5</span>
+                            <span class="text-md  block">20 ratings</span>
                         </div>
-                    </div>
-                    <p class="review_title font-bold text-lg pl-12">Nice graphics</p>
-                    <img class="review_photo mt-3 pl-12" src="../resources/img/reviewProduct/images 5.png" alt="">
-                    <p class="review_text mt-3">Lorem ipsum dolor sit amet consectetur. Eu dictumst orci egestas vitae donec. Pretium quis duis aliquam feugiat. Arcu commodo faucibus facilisis ut amet duis nullam tincidunt egestas. Senectus elit dignissim in cras nibh.</p>
-                </div>
-                <div class="replies mt-2 pl-10">
-                    <div class="bg-productCardBgColor py-5 px-5 shadow-md">
-                        <div class="font-bold text-lg mb-3"><ion-icon class="mr-2" name="arrow-undo-sharp"></ion-icon>Replied</div>
-                        <div class="flex justify-between items-center">
-                            <!-- user img and rating star img -->
-                            <div class="flex justify-start items-center">
-                                <img class="w-10" src="../resources/img/reviewProduct/c2 3.png" alt="">
-                                <img class="ml-2 w-24 review_userRatingImg" src="../resources/img/reviewProduct/4stars.svg" alt="">
+
+                        <div class="flex flex-col">
+                            <div class="rating text">
+                                <span class="starColor">&#9733;</span>
+                                <span class="starColor">&#9733;</span>
+                                <span class="starColor">&#9733;</span>
+                                <span class="starColor">&#9733;</span>
+                                <span class="starColor">&#9733;</span>
+                                <span class="">15</span>
+
+
                             </div>
-                            <!-- Date review written -->
-                            <div>
-                                <p class="review_createDate text-lg">2023/07/20</p>
+
+                            <div class="rating text">
+                                <span class="starColor">&#9733;</span>
+                                <span class="starColor">&#9733;</span>
+                                <span class="starColor">&#9733;</span>
+                                <span class="starColor">&#9733;</span>
+                                <span class="">&#9733;</span>
+                                <span class="">5</span>
+
+
+                            </div>
+
+                            <div class="rating text">
+                                <span class="starColor">&#9733;</span>
+                                <span class="starColor">&#9733;</span>
+                                <span class="starColor">&#9733;</span>
+                                <span class="">&#9733;</span>
+                                <span class="">&#9733;</span>
+                                <span></span>
+                            </div>
+
+                            <div class="rating text">
+                                <span class="starColor">&#9733;</span>
+                                <span class="starColor">&#9733;</span>
+                                <span class="">&#9733;</span>
+                                <span class="">&#9733;</span>
+                                <span class="">&#9733;</span>
+                                <span></span>
+                            </div>
+
+
+                            <div class="rating text">
+                                <span class="starColor">&#9733;</span>
+                                <span class="">&#9733;</span>
+                                <span class="">&#9733;</span>
+                                <span class="">&#9733;</span>
+                                <span class="">&#9733;</span>
+                                <span></span>
                             </div>
                         </div>
-                        <p class="review_title font-bold text-lg pl-12">Nice graphics</p>
-                        <img class="review_photo mt-3 pl-12" src="../resources/img/item_detail/images 6.svg" alt="">
-                        <p class="review_text mt-3">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Rerum, laborum?</p>
+
                     </div>
                 </div>
+
+
             </div>
 
-            <div class="mt-5">
-                <div class="bg-productCardBgColor py-5 px-5 shadow-md">
-                    <div class="flex justify-between items-center">
-                        <!-- user img and rating star img -->
-                        <div class="flex justify-start items-center">
-                            <img class="w-10" src="../resources/img/item_detail/c2 4.svg" alt="">
-                            <img class="ml-2 w-24 review_userRatingImg" src="../resources/img/item_detail/4stars.svg" alt="">
+
+            <div class="w-[900px] h-[100px] mx-auto bg-[#F7F7F7] shadow-sm">
+                <div class="w-[900px] h-[30px] relative px-4 py-4">
+                    <div class="profile flex ">
+                        <div class="w-[30px] h-[30px] rounded-full mt-1">
+                            <img class="leading-[30px]" src="../resources/img/reviewProduct/c2 3.png" alt="">
+                            <div class="rating text flex ">
+                                <span class="starColor">&#9733;</span>
+                                <span class="starColor">&#9733;</span>
+                                <span class="starColor">&#9733;</span>
+                                <span class="starColor">&#9733;</span>
+                                <span class="">&#9733;</span>
+
+
+
+                            </div>
                         </div>
-                        <!-- Date review written -->
+
+                        <span class=" font-semibold text-lg p-2">User A</span>
+
+
                         <div>
-                            <p class="review_createDate text-lg">2023/07/20</p>
+                            <span class="absolute right-0">2023/07/12</span>
+                            <button class="replyBtn px-5 rounded-sm py-1 bg-[#304547] text-white absolute right-0 top-10" >Reply</button>
                         </div>
                     </div>
-                    <p class="review_title font-bold text-lg pl-12">Nice graphics</p>
-                    <img class="review_photo mt-3 pl-12" src="" alt="">
-                    <p class="review_text mt-3">Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque quam odio quibusdam nesciunt reprehenderit consequuntur ab, adipisci labore unde commodi.</p>
+                    <span class="mt-3 block">Lorem ipsum dolor sit amet consectetur. Eu dictumst orci egestas vitae donec. </span>
                 </div>
+
+
             </div>
+
+            <!-- reply review -->
+            <div class="w-[850px] h-[100px]  ml-[100px] bg-[#F7F7F7] mt-2 shadow-sm px-4 py-4 hidden" id="reply">
+
+                <div>
+                    <span class="text-md font-semibold">
+                        <img class="inline" src="../resources/img/reviewProduct/arrow.svg" alt="">
+                        Reply to UserA</span>
+                </div>
+                <div class="mt-1">
+                    <input type="text" placeholder="Message" class="w-[320px] h-[40px] border border-black rounded-sm ml-5 pl-4">
+                    <button class="replySend px-5 rounded-sm py-1 bg-[#304547] text-white">Reply</button>
+                </div>
+
+            </div>
+
+            <div class="w-[900px] h-[100px] mx-auto bg-[#F7F7F7] mt-2 shadow-sm">
+                <div class="w-[900px] h-[30px] relative px-4 py-4">
+                    <div class="profile flex ">
+                        <div class="w-[30px] h-[30px] rounded-full mt-1">
+                            <img class="leading-[30px]" src="../resources/img/reviewProduct/c2 3.png" alt="">
+                            <div class="rating text flex">
+                                <span class="starColor">&#9733;</span>
+                                <span class="starColor">&#9733;</span>
+                                <span class="starColor">&#9733;</span>
+                                <span class="starColor">&#9733;</span>
+                                <span class="">&#9733;</span>
+
+
+
+                            </div>
+                        </div>
+
+                        <span class=" font-semibold text-lg ">User A</span>
+
+
+                        <div>
+                            <span class="absolute right-0">2023/07/12</span>
+                            <button class=" px-5 rounded-sm py-1 bg-[#304547] text-white absolute right-0 top-10">Reply</button>
+                        </div>
+                    </div>
+                    <span class="mt-3 block">Lorem ipsum dolor sit amet consectetur. Eu dictumst orci egestas vitae donec. </span>
+                </div>
+
+
+            </div>
+
+
+
         </div>
-        <!-- End of user reviews -->
     </div>
 
+
+    <!-- end review -->
+    <script>
+         $(document).ready(function() {
+            $(".replyBtn").click(function() {
+                // Hide all other screens and show screenOne
+
+                $("#reply").removeClass("hidden");
+                
+
+
+            });
+            $(".replySend").click(function() {
+                // Hide all other screens and show screenOne
+
+                $("#reply").addClass("hidden");
+                $(".replyBtn").removeClass("hidden");
+            
+                
+
+
+            });
+        });
+    </script>
 </body>
 
 </html>
