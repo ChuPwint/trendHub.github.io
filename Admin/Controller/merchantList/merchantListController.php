ff08c84649d00b26638fe29b346a8929fde005de
Chu <chupwintchal99@gmail.com>
2023-08-02 13:02:29 +0630

    controller merchant list update

--- Admin/Controller/merchantList/merchantListController.php
+++ Admin/Controller/merchantList/merchantListController.php
@@ -9,37 +9,37 @@ if(($_GET["name"]) == "allMerchant"){
     $sql->execute();
     $allMerchantList = $sql->fetchAll(PDO::FETCH_ASSOC);
     $_SESSION["allMerchantList"] = $allMerchantList;
-    //print_r($allMerchantList);
-    //print_r($_SESSION["allMerchantList"]);
     header("Location: ../../View/merchantList/allMerchant.php");
 }
 
+if(($_GET["name"]) == "newMerchant"){
+    $todayDate = date("Y-m-d");
+    $sql = $pdo->prepare(
+        "SELECT * FROM m_marchents WHERE create_date = :date"
+    );
+    $sql->bindValue(":date", $todayDate);
+    $sql->execute();
+    $newMerchantList = $sql->fetchAll(PDO::FETCH_ASSOC);
+    $_SESSION["newMerchantList"] = $newMerchantList;
+    header("Location: ../../View/merchantList/newMerchant.php");
+}
 
-$todayDate = date("Y-m-d");
-$sql = $pdo->prepare(
-    "SELECT * FROM m_marchents WHERE create_date = :date"
-);
-$sql->bindValue(":date", $todayDate);
-$sql->execute();
-$newMerchantList = $sql->fetchAll(PDO::FETCH_ASSOC);
+if(($_GET["name"]) == "pendingMerchant"){
+    $sql = $pdo->prepare(
+        "SELECT * FROM m_marchents WHERE approval = 0"
+    );
+    $sql->execute();
+    $pendingMerchantList = $sql->fetchAll(PDO::FETCH_ASSOC);
+    $_SESSION["pendingMerchantList"] = $pendingMerchantList;
+    header("Location: ../../View/merchantList/pendingMerchant.php");
+}
 
-$sql = $pdo->prepare(
-    "SELECT * FROM m_marchents WHERE approval = 0"
-);
-$sql->execute();
-$pendingMerchantList = $sql->fetchAll(PDO::FETCH_ASSOC);
-
-$sql = $pdo->prepare(
-    "SELECT * FROM m_marchents WHERE banned = 1"
-);
-$sql->execute();
-$bannedMerchantList = $sql->fetchAll(PDO::FETCH_ASSOC);
-
-// echo "<pre>";
-// echo $todayDate;
-// print_r($allMerchantList);
-// print_r($newMerchantList);
-// print_r($pendingMerchantList);
-// print_r($bannedMerchantList);
-
-?>
+if(($_GET["name"]) == "bannedMerchant"){
+    $sql = $pdo->prepare(
+        "SELECT * FROM m_marchents WHERE banned = 1"
+    );
+    $sql->execute();
+    $bannedMerchantList = $sql->fetchAll(PDO::FETCH_ASSOC);
+    $_SESSION["bannedMerchantList"] = $bannedMerchantList;
+    header("Location: ../../View/merchantList/bannedMerchant.php");
+}
