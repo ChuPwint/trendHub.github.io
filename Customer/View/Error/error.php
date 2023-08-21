b9c5c9b21851bfb5fd1de71da0320dee6a43159e
ChuPwint <chupwintchal99@gmail.com>
2023-08-21 22:35:15 +0630

    contact fix, error page link fix

--- Customer/View/Error/error.php
+++ Customer/View/Error/error.php
@@ -48,7 +48,7 @@
         <span class=" text-[120px] text-black">4</span>
    </div>
    <span class="text-black text-xl">Whoops.. Cart Not Found!!</span>
-   <a href="../test.php">
+   <a href="../index.php">
     <button class="md:mt-7 block mx-auto text-xl px-[30px] rounded-md mt-[100px] py-[6px] bg-black text-white hover:bg-tertiary">Go Home</button>
    </a>
    <img class="md:block hidden absolute -right-[28px] -bottom-[16px] -rotate-[20deg] vert-move3" src="../resources/img/error/laura-chouette.svg" alt="">
