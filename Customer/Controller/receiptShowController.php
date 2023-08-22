9fc0409f756fae69df61f69ce6bc5de3289cf6c7
DESKTOP-UK4Q25M\Ma Thazin Aung <taung2459@gmail.com>
2023-08-22 14:33:41 +0630

    no message

--- Customer/Controller/receiptShowController.php
+++ Customer/Controller/receiptShowController.php
@@ -1,9 +1,9 @@
 <?php
 session_start();
 
-if(!isset($_SESSION["orderId"])){
+if (!isset($_SESSION["orderId"])) {
     header("Location: ../../View/Error/error.php");
-}else{
+} else {
     $orderId = $_SESSION["orderId"];
 
     include "../../Model/model.php";
@@ -18,8 +18,8 @@ if(!isset($_SESSION["orderId"])){
     $sql->bindValue(":id", $orderId);
     $sql->execute();
     $selectedOrder = $sql->fetchAll(PDO::FETCH_ASSOC);
-  
-    
+
+
 
     $sql2 = $pdo->prepare(
         "SELECT t_order_details.*, m_products.* 
@@ -28,10 +28,72 @@ if(!isset($_SESSION["orderId"])){
         WHERE t_order_details.order_id = :id;
         )"
     );
-   
+
     $sql2->bindValue(":id", $orderId);
     $sql2->execute();
-    $orderDetails= $sql2->fetchAll(PDO::FETCH_ASSOC);
+    $orderDetails = $sql2->fetchAll(PDO::FETCH_ASSOC);
+    //to notify to merchants for new order
+
+    $sql = $pdo->prepare(
+        "SELECT m_marchents.*, m_marchents.id AS merchantId, t_orders.*
+    FROM m_marchents
+    JOIN  t_orders ON  t_orders.merchant_id = m_marchents.id
+    WHERE  t_orders.id = :id;"
+    );
+    $sql->bindValue(":id", $orderId);
+    $sql->execute();
+    $merchant = $sql->fetchAll(PDO::FETCH_ASSOC);
+    $merchantId = $merchant[0]['merchantId'];
+    $customerId = $merchant[0]['customer_id'];
+   
+
+    $merchantName =  $merchant[0]['m_name'];
+
+    $title = "New Order for Your Products ,Dear [ $merchantName]";
+    $text = "A new order has been placed for your products. Please review the order details and prepare for delivery.";
+    if (($merchant[0]['merchantId']) == 1) {
+        $sql = $pdo->prepare(
+            "INSERT INTO t_contact_customers
+        (
+          customer_id,
+          msg_text,
+          create_date
+        )
+        VALUES
+        (
+          :customerID,
+          :msg_text,
+          :create_date
+        )
+        "
+        );
+
+        $sql->bindValue(":customerID", $customerId);
+        $sql->bindValue(":msg_text", $text);
+        $sql->bindValue(":create_date", date("Y-m-d"));
+        $sql->execute();
+    } else {
+        $sql = $pdo->prepare(
+            "INSERT INTO t_notify_to_merchant
+        (
+           merchant_id,
+           title,
+           message,
+           create_date
+        )
+        VALUES
+        (
+           :merchant_id,
+           :title,
+           :message,
+           :create_date
+        )
+        "
+        );
+        $sql->bindValue(":merchant_id", $merchantId);
+        $sql->bindValue(":title", $title);
+        $sql->bindValue(":message", $text);
+        $sql->bindValue(":create_date", date("Y-m-d"));
+        $sql->execute();
+    }
 }
-    
-?>
