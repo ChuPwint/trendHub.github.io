ff4fe44115afaf34842a2707b3b719cdda4b3b43
DESKTOP-UK4Q25M\Ma Thazin Aung <taung2459@gmail.com>
2023-08-23 09:04:17 +0630

    no message

--- Merchant/View/resources/common/commonFunction.php
+++ Merchant/View/resources/common/commonFunction.php
@@ -14,6 +14,18 @@ function getToken($length) {
     return $randomString;
 }
 
+// Verify email token function 
+function getVerifyEmailToken($length = 4) {
+    $character = "0123456789";
+    $randomString = "";
+
+    for ($i=0; $i < $length; $i++) { 
+        $index = rand(0, strlen($character) - 1);
+        $randomString .= $character[$index];
+    }
+
+    return $randomString;
+}
 
 
 
