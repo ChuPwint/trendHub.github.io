2b4a5c365585182799a68bab08f93a512d744fcf
ChuPwint <chupwintchal99@gmail.com>
2023-08-23 16:45:36 +0630

    change modal database

--- Merchant/Model/model.php
+++ Merchant/Model/model.php
@@ -1,10 +1,11 @@
 <?php
 
-$hostname = "localhost";
-$port = 3306;
+$hostname = "trendhub-do-user-14558665-0.b.db.ondigitalocean.com";
+$port = 25060;
 $dbname = "trendhub_db";
-$username = "root";
-$password = "";
+$username = "doadmin";
+$password = "AVNS_HBDFdjOSHAfqV3IwBDO";
+
 
 //Initial Connection
 $pdo = new PDO("mysql:host=$hostname;port=$port;dbname=$dbname", $username,$password);
